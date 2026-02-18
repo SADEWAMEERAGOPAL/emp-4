d160d71fcdce89d194452ebe83fded655b366927
SADEWAMEERAGOPAL <SADEWADMEERA76@GMAIL.COM>
2026-02-19 01:52:36 +0530

    fix project root structure

--- emp-4/src/app/shared/model/employee.ts
+++
@@ -1,9 +0,0 @@
-export interface IEmployee{
-  Fname: string,
-  Lname: string,
-  Email: string,
-  Contact: number,
-  Role: string,
-  id: string
-  Address: string
-}
--- emp-4/src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-   Baseurl: "https://subject-crud-a1f8a-default-rtdb.firebaseio.com"
-};
